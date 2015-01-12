99804c029d0fb1cfdda552836d135cbae947702d
maya <maya@codeforamerica.org>
2015-01-12 00:28:13 -0800

    First commit on revisr

---
+++ wp-content/plugins/revisr/includes/class-revisr-admin.php
@@ -0,0 +1,287 @@
+<?php
+/**
+ * class-revisr-admin.php
+ *
+ * Handles admin-specific functionality.
+ *
+ * @package   Revisr
+ * @license   GPLv3
+ * @link      https://revisr.io
+ * @copyright 2014 Expanded Fronts, LLC
+ */
+
+// Disallow direct access.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Revisr_Admin {
+
+	/**
+	 * The main database class.
+	 * @var Revisr_DB()
+	 */
+	protected $db;
+
+	/**
+	 * The main Git class.
+	 * @var Revisr_Git()
+	 */
+	protected $git;
+
+	/**
+	 * User options and preferences.
+	 * @var array
+	 */
+	protected $options;
+
+	/**
+	 * Initialize the class.
+	 * @access public
+	 */
+	public function __construct() {
+		$revisr 		= Revisr::get_instance();
+		$this->db 		= $revisr->db;
+		$this->git 		= $revisr->git;
+		$this->options 	= Revisr::get_options();
+	}
+
+	/**
+	 * Stores an alert to be rendered on the dashboard.
+	 * @access public
+	 * @param  string  $message 	The message to display.
+	 * @param  bool    $is_error Whether the message is an error.
+	 */
+	public static function alert( $message, $is_error = false ) {
+		if ( $is_error == true ) {
+			set_transient( 'revisr_error', $message, 10 );
+		} else {
+			set_transient( 'revisr_alert', $message, 3 );
+		}
+	}
+
+	/**
+	 * Returns the data for the AJAX buttons.
+	 * @access public
+	 */
+	public function ajax_button_count() {
+		if ( $_REQUEST['data'] == 'unpulled' ) {
+			echo $this->git->count_unpulled();
+		} else {
+			echo $this->git->count_unpushed();
+		}
+		exit();
+	}
+
+	/**
+	 * Deletes existing transients.
+	 * @access public
+	 */
+	public static function clear_transients( $errors = true ) {
+		if ( $errors == true ) {
+			delete_transient( 'revisr_error' );
+		} else {
+			delete_transient( 'revisr_alert' );
+		}
+	}
+
+	/**
+	 * Counts the number of commits in the database on a given branch.
+	 * @access public
+	 * @param  string $branch The name of the branch to count commits for.
+	 */
+	public static function count_commits( $branch ) {
+		global $wpdb;
+		if ( $branch == 'all' ) {
+			$num_commits = $wpdb->get_results( "SELECT * FROM " . $wpdb->postmeta . " WHERE meta_key = 'branch'" );
+		} else {
+			$num_commits = $wpdb->get_results( "SELECT * FROM " . $wpdb->postmeta . " WHERE meta_key = 'branch' AND meta_value = '".$branch."'" );
+		}
+		return count( $num_commits );
+	}
+
+	/**
+	 * Logs an event to the database.
+	 * @access public
+	 * @param  string $message The message to show in the Recent Activity. 
+	 * @param  string $event   Will be used for filtering later. 
+	 */
+	public static function log( $message, $event ) {
+		global $wpdb;
+		$time  = current_time( 'mysql' );
+		$table = $wpdb->prefix . 'revisr';
+		$wpdb->insert(
+			"$table",
+			array( 
+				'time' 		=> $time,
+				'message'	=> $message,
+				'event' 	=> $event,
+			),
+			array(
+				'%s',
+				'%s',
+				'%s',
+			)
+		);		
+	}
+
+	/**
+	 * Notifies the admin if notifications are enabled.
+	 * @access private
+	 * @param  string $subject The subject line of the email.
+	 * @param  string $message The message for the email.
+	 */
+	public static function notify( $subject, $message ) {
+		$options 	= Revisr::get_options();
+		$url 		= get_admin_url() . 'admin.php?page=revisr';
+
+		if ( isset( $options['notifications'] ) ) {
+			$email 		= $options['email'];
+			$message	.= '<br><br>';
+			$message	.= sprintf( __( '<a href="%s">Click here</a> for more details.', 'revisr' ), $url );
+			$headers 	= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+			wp_mail( $email, $subject, $message, $headers );
+		}
+	}
+
+	/**
+	 * Renders an alert and removes the old data. 
+	 * @access public
+	 */
+	public function render_alert() {
+		$alert = get_transient( 'revisr_alert' );
+		$error = get_transient( 'revisr_error' );
+		if ( $error ) {
+			echo "<div class='revisr-alert error'>" . wpautop( $error ) . "</div>";
+		} else if ( $alert ) {
+			echo "<div class='revisr-alert updated'>" . wpautop( $alert ) . "</div>";
+		} else {
+			if ( $this->git->count_untracked() == '0' ) {
+				printf( __( '<div class="revisr-alert updated"><p>There are currently no untracked files on branch %s.', 'revisr' ), $this->git->branch );
+			} else {
+				$commit_link = get_admin_url() . 'post-new.php?post_type=revisr_commits';
+				printf( __('<div class="revisr-alert updated"><p>There are currently %s untracked files on branch %s. <a href="%s">Commit</a> your changes to save them.</p></div>', 'revisr' ), $this->git->count_untracked(), $this->git->branch, $commit_link );
+			}
+		}
+		exit();
+	}
+
+	/**
+	 * Processes a diff request.
+	 * @access public
+	 */
+	public function view_diff() {
+		?>
+		<html>
+		<head><title><?php _e( 'View Diff', 'revisr' ); ?></title>
+		</head>
+		<body>
+		<?php
+			if ( isset( $_REQUEST['commit'] ) ) {
+				$diff = $this->git->run("show {$_REQUEST['commit']} {$_REQUEST['file']}");
+			} else {
+				$diff = $this->git->run("diff {$_REQUEST['file']}");
+			}
+			if ( is_array( $diff ) ) {
+				foreach ( $diff as $line ) {
+					if (substr( $line, 0, 1 ) === "+") {
+						echo "<span class='diff_added' style='background-color:#cfc;'>" . htmlspecialchars($line) . "</span><br>";
+					} else if (substr( $line, 0, 1 ) === "-") {
+						echo "<span class='diff_removed' style='background-color:#fdd;'>" . htmlspecialchars($line) . "</span><br>";
+					} else {
+						echo htmlspecialchars($line) . "<br>";
+					}	
+				}			
+			} else {
+				_e( 'Failed to render the diff.', 'revisr' );
+			}
+		?>
+		</body>
+		</html>
+		<?php
+		exit();
+	}
+
+	/**
+	 * Shows a list of the pending files on the current branch. Clicking a modified file shows the diff.
+	 * @access public
+	 */
+	public function pending_files() {
+		check_ajax_referer('pending_nonce', 'security');
+		$output 		= $this->git->status();
+		$total_pending 	= count( $output );
+		$text 			= sprintf( __( 'There are <strong>%s</strong> untracked files that can be added to this commit on branch <strong>%s</strong>.', 'revisr' ), $total_pending, $this->git->branch );
+		echo "<br>" . $text . "<br><br>";
+		_e( 'Use the boxes below to select the files to include in this commit. Only files in the "Staged Files" section will be included.<br>Double-click files marked as "Modified" to view the changes to the file.<br><br>', 'revisr' );
+		echo "<input id='backup_db_cb' type='checkbox' name='backup_db'><label for='backup_db_cb'>" . __( 'Backup database?', 'revisr' ) . "</label><br><br>";
+
+		if ( is_array( $output ) ) {
+				?>
+				<!-- Staging -->
+				<div class="stage-container">
+					<p><strong><?php _e( 'Staged Files', 'revisr' ); ?></strong></p>
+					<select id='staged' multiple="multiple" name="staged_files[]" size="6">
+					<?php
+					// Clean up output from git status and echo the results.
+					foreach ( $output as $result ) {
+						$short_status = substr( $result, 0, 3 );
+						$file = substr( $result, 3 );
+						$status = Revisr_Git::get_status( $short_status );
+						echo "<option class='pending' value='{$result}'>{$file} [{$status}]</option>";
+					}
+					?>
+					</select>
+					<div class="stage-nav">
+						<input id="unstage-file" type="button" class="button button-primary stage-nav-button" value="<?php _e( 'Unstage Selected', 'revisr' ); ?>" onclick="unstage_file()" />
+						<br>
+						<input id="unstage-all" type="button" class="button stage-nav-button" value="<?php _e( 'Unstage All', 'revisr' ); ?>" onclick="unstage_all()" />
+					</div>
+				</div><!-- /Staging -->		
+				<br>
+				<!-- Unstaging -->
+				<div class="stage-container">
+					<p><strong><?php _e( 'Unstaged Files', 'revisr' ); ?></strong></p>
+					<select id="unstaged" multiple="multiple" size="6">
+					</select>
+					<div class="stage-nav">
+						<input id="stage-file" type="button" class="button button-primary stage-nav-button" value="<?php _e( 'Stage Selected', 'revisr' ); ?>" onclick="stage_file()" />
+						<br>
+						<input id="stage-all" type="button" class="button stage-nav-button" value="<?php _e( 'Stage All', 'revisr' ); ?>" onclick="stage_all()" />
+					</div>
+				</div><!-- /Unstaging -->
+			<?php	
+		}	
+		exit();
+	}
+
+	/**
+	 * Shows the files that were added in a given commit.
+	 * @access public
+	 */
+	public function committed_files() {
+		if ( get_post_type( $_POST['id'] ) != 'revisr_commits' ) {
+			exit();
+		}
+		check_ajax_referer( 'committed_nonce', 'security' );
+		$committed_files = get_post_custom_values( 'committed_files', $_POST['id'] );
+		$commit_hash	 = get_post_custom_values( 'commit_hash', $_POST['id'] );
+		if ( is_array( $committed_files ) ) {
+			foreach ( $committed_files as $file ) {
+				$output = maybe_unserialize( $file );
+			}
+		}
+		if ( isset( $output ) ) {
+			printf( __('<br><strong>%s</strong> files were included in this commit. Double-click files marked as "Modified" to view the changes in a diff.', 'revisr' ), count( $output ) );
+			echo "<input id='commit_hash' name='commit_hash' value='{$commit_hash[0]}' type='hidden' />";
+			echo '<br><br><select id="committed" multiple="multiple" size="6">';
+				foreach ( $output as $result ) {
+					$short_status = substr( $result, 0, 3 );
+					$file = substr($result, 2);
+					$status = Revisr_Git::get_status( $short_status );
+					printf( '<option class="committed" value="%s">%s [%s]</option>', $result, $file, $status );	
+				}
+			echo '</select>';
+		} else {
+			_e( 'No files were included in this commit.', 'revisr' );
+		}
+		exit();
+	}	
+}
---
+++ wp-content/plugins/revisr/includes/class-revisr-git.php
@@ -0,0 +1,546 @@
+<?php
+/**
+ * class-revisr-git.php
+ *
+ * Processes Git functions.
+ *
+ * @package   Revisr
+ * @license   GPLv3
+ * @link      https://revisr.io
+ * @copyright 2014 Expanded Fronts, LLC
+ */
+
+// Disallow direct access.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Revisr_Git {
+
+	/**
+	 * The current branch of the local repository.
+	 * @var string
+	 */
+	public $branch;
+
+	/**
+	 * The top-level Git directory.
+	 * @var string
+	 */
+	public $dir;
+
+	/**
+	 * The short SHA1 hash of the current state of the repository.
+	 * @var string
+	 */
+	public $hash;
+
+	/**
+	 * User options and preferences.
+	 * @var array
+	 */
+	public $options;
+
+	/**
+	 * The name of the active remote.
+	 * @var string
+	 */
+	public $remote;
+
+	/**
+	 * Initiate the class properties.
+	 * @access public
+	 */
+	public function __construct() {
+		$this->dir 		= $this->current_dir();
+		$this->options  = Revisr::get_options();
+
+		if ( $this->is_repo() ) {
+			$this->branch 	= $this->current_branch();
+			$this->remote 	= $this->current_remote();
+			$this->hash 	= $this->current_commit();			
+		}
+	}
+
+	/**
+	 * Pushes changes if "Automatically push new commits" is enabled.
+	 * @access public
+	 */
+	public function auto_push() {
+		if ( $this->config_revisr_option( 'auto-push' ) === 'true' ) {
+			$this->push();
+		}
+	}
+
+	/**
+	 * Checks out an existing branch.
+	 * @access public
+	 * @param string $branch The branch to checkout.
+	 */
+	public function checkout( $branch ) {
+		$this->run( "checkout $branch", __FUNCTION__ );
+	}
+
+	/**
+	 * Commits any staged files to the local repository.
+	 * @access public
+	 * @param  string $message 		The message to use with the commit.
+	 * @param  string $callback 	The callback to run.
+	 */
+	public function commit( $message, $callback = '' ) {
+		$commit_message = escapeshellarg($message);
+		$commit 		= $this->run( "commit -m$commit_message", $callback );
+		return $commit;
+	}
+
+	/**
+	 * Gets or sets the user's email address stored in Git.
+	 * @access public
+	 * @param  string $user_email If provided, will update the user's email.
+	 */
+	public function config_user_email( $user_email = '' ) {
+		$email = $this->run( "config user.email $user_email" );
+		return $email;
+	}
+
+	/**
+	 * Gets or sets the username stored in Git.
+	 * @access public
+	 * @param  string $username If provided, will update the username.
+	 */
+	public function config_user_name( $username = '' ) {
+		$username = $this->run( "config user.name $username" );
+		return $username;
+	}
+
+	/**
+	 * Stores or retrieves options into the 'revisr' block of the '.git/config'.
+	 * This is necessary for Revisr to be environment agnostic, even if the 'wp_options'
+	 * table is tracked and subsequently imported.
+	 * @access public
+	 * @param  string $option 	The name of the option to store.
+	 * @param  string $value 	The value of the option to store.
+	 */
+	public function config_revisr_option( $option, $value = '' ) {
+		if ( $value != '' ) {
+			$this->run( "config revisr.$option $value" );
+		}
+
+		// Retrieve the data for verification/comparison.
+		$data = $this->run( "config revisr.$option" );
+		if ( is_array( $data ) ) {
+			return $data[0];
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Stores URLs for Revisr to the .git/config (to be environment-agnostic).
+	 * @access public
+	 * @param  string $env The associated environment.
+	 * @param  string $url The URL to store.
+	 * @return string|boolean
+	 */
+	public function config_revisr_url( $env, $url = '' ) {
+		if ( $url != '' ) {
+			$this->run( "config revisr.$env-url $url" );
+		}
+
+		// Retrieve the URL for using elsewhere.
+		$data = $this->run( "config revisr.$env-url" );
+		if ( is_array( $data ) ) {
+			return $data[0];
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Stores environment paths to .git/config (to be environment-agnostic).
+	 * @access public
+	 * @param  string $service 	For ex., git or mysql
+	 * @param  string $path 	The path to store.
+	 */
+	public function config_revisr_path( $service, $path = '' ) {
+		$revisr_path = $this->run( "config revisr.$service-path $path" );
+		return $revisr_path;
+	}
+
+	/**
+	 * Returns the number of unpulled commits.
+	 * @access public
+	 */
+	public function count_unpulled( $ajax_btn = true ) {
+		$this->fetch();
+		if ( $ajax_btn == true ) {
+			$this->run( "log {$this->branch}..{$this->remote}/{$this->branch} --pretty=oneline", 'count_ajax_btn' );
+		} else {
+			$unpulled = $this->run( "log {$this->branch}..{$this->remote}/{$this->branch} --pretty=oneline" );
+			return count( $unpulled );
+		}
+	}
+
+	/**
+	 * Returns the number of unpushed commits.
+	 * @access public
+	 */
+	public function count_unpushed( $ajax_btn = true ) {
+		if ( $ajax_btn == true ) {
+			$this->run("log {$this->remote}/{$this->branch}..{$this->branch} --pretty=oneline", 'count_ajax_btn' );
+		} else {
+			$unpushed = $this->run("log {$this->remote}/{$this->branch}..{$this->branch} --pretty=oneline" );
+			return count( $unpushed );
+		}
+	}
+
+	/**
+	 * Returns the number of untracked/modified files.
+	 * @access public
+	 */
+	public function count_untracked() {
+		$untracked = $this->run( 'status --short' );
+		return count( $untracked );
+	}
+
+	/**
+	 * Creates a new branch.
+	 * @access public
+	 * @param  string $branch The name of the branch to create.
+	 */
+	public function create_branch( $branch ) {
+		$new_branch = $this->run( "branch $branch" );
+		return $new_branch;
+	}	
+
+	/**
+	 * Returns the current branch.
+	 * @access public
+	 */
+	public function current_branch() {
+		$current_branch = $this->run( 'rev-parse --abbrev-ref HEAD' );
+		if ( $current_branch != false ) {
+			return $current_branch[0];
+		}
+	}
+
+	/**
+	 * Returns the hash of the current commit.
+	 * @access public
+	 */
+	public function current_commit() {
+		$commit_hash = $this->run( 'rev-parse --short HEAD' );
+		if ( is_array( $commit_hash ) ) {
+			return $commit_hash[0];
+		}
+	}
+
+	/**
+	 * Returns the path to the top-level git directory.
+	 * @access public
+	 * @return string The path to the top-level Git directory.
+	 */
+	public function current_dir() {
+		$dir = exec( 'git rev-parse --show-toplevel' );
+		if ( $dir ) {
+			return $dir;
+		} else {
+			return ABSPATH;
+		}
+	}
+
+	/**
+	 * Returns the name of the current remote.
+	 * @access public
+	 */
+	public function current_remote() {
+		if ( isset( $this->options['remote_name'] ) && $this->options['remote_name'] != '' ) {
+			return $this->options['remote_name'];
+		} else {
+			return 'origin';
+		}
+	}
+
+	/**
+	 * Deletes a branch.
+	 * @access public
+	 * @param  string $branch The branch to delete.
+	 */
+	public function delete_branch( $branch ) {
+		$deletion = $this->run( "branch -D $branch", __FUNCTION__, $branch );
+		return $deletion;
+	}
+
+	/**
+	 * Fetches changes without merging them.
+	 * @access public
+	 */
+	public function fetch() {
+		$fetch = $this->run( 'fetch' );
+		return $fetch;
+	}
+
+	/**
+	 * Returns available branches on the local or remote repository.
+	 * @access public
+	 * @param  boolean $remote If set to true, will retrieve the remote branches.
+	 */
+	public function get_branches( $remote = false ) {
+		if ( $remote == true ) {
+			$branches = $this->run( 'branch -r' );
+		} else {
+			$branches = $this->run( 'branch' );
+		}
+		return $branches;
+	}
+
+	/**
+	 * Returns the commit hash for a specific commit.
+	 * @access public
+	 * @param  int $post_id The ID of the associated post.
+	 */
+	public static function get_hash( $post_id ) {
+		$commit_meta = maybe_unserialize( get_post_meta( $post_id, "commit_hash" ) );		
+		if ( isset( $commit_meta[0] ) ) {
+			if ( ! is_array( $commit_meta[0] ) && strlen( $commit_meta[0] ) == "1" ) {
+				$commit_hash = $commit_meta;
+			}
+			else {
+				$commit_hash = $commit_meta[0];
+			}
+		}
+		if ( empty( $commit_hash ) ) {
+			return __( 'Unknown', 'revisr' );
+		} else {
+			if ( is_array( $commit_hash ) ) {
+				return $commit_hash[0];
+			} else {
+				return $commit_hash;
+			}
+		}
+	}
+
+	/**
+	 * Returns the status of a file.
+	 * @access public
+	 * @param  string $status The status code returned via 'git status --short'
+	 */
+	public static function get_status( $status ) {
+		if ( strpos( $status, 'M' ) !== false ){
+			$status = __( 'Modified', 'revisr' );
+		} elseif ( strpos( $status, 'D' ) !== false ){
+			$status = __( 'Deleted', 'revisr' );
+		} elseif ( strpos( $status, 'A' ) !== false ){
+			$status = __( 'Added', 'revisr' );
+		} elseif ( strpos( $status, 'R' ) !== false ){
+			$status = __( 'Renamed', 'revisr' );
+		} elseif ( strpos( $status, 'U' ) !== false ){
+			$status = __( 'Updated', 'revisr' );
+		} elseif ( strpos( $status, 'C' ) !== false ){
+			$status = __( 'Copied', 'revisr' );
+		} elseif ( strpos( $status, '??' ) !== false ){
+			$status = __( 'Untracked', 'revisr' );
+		} else {
+			$status = false;
+		}
+		return $status;
+	}
+
+	/**
+	 * Initializes a new repository.
+	 * @access public
+	 */
+	public function init_repo() {
+		$init = $this->run( 'init .', __FUNCTION__ );
+		return $init;
+	}
+
+	/**
+	 * Checks if a given branch name exists in the local repository.
+	 * @access public
+	 * @param  string $branch The branch to check.
+	 */
+	public function is_branch( $branch ) {
+		$branches = $this->get_branches();
+		if ( in_array( $branch, $branches ) || in_array( "* $branch", $branches ) || in_array( "  $branch", $branches ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Checks if the WordPress install is in a Git repository.
+	 * @access public
+	 */
+	public function is_repo() {
+		exec( 'git rev-parse --show-toplevel', $output, $error );
+		if ( $error ) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * Merges a branch into the current branch.
+	 * @access public
+	 * @param  string $branch The branch to merge into the current branch.
+	 */
+	public function merge( $branch ) {
+		$this->reset();
+		$merge = $this->run( "merge $branch --strategy-option theirs", __FUNCTION__ );
+		return $merge;
+	}
+
+	/**
+	 * Pulls changes from the remote repository.
+	 * @access public
+	 */
+	public function pull() {
+		$this->reset();
+		$pull = $this->run( "pull -Xtheirs --quiet {$this->remote} {$this->branch}", __FUNCTION__, $this->count_unpulled( false ) );
+		return $pull;
+	}
+
+	/**
+	 * Pushes changes to the remote repository.
+	 * @access public
+	 */
+	public function push() {
+		$push = $this->run( "push {$this->remote} HEAD --quiet", __FUNCTION__, $this->count_unpushed( false ) );
+		return $push;
+	}
+
+	/**
+	 * Resets the working directory.
+	 * @access public
+	 * @param  string 	$mode	The mode to use for the reset (hard, soft, etc.).
+	 * @param  string 	$path 	The path to apply the reset to.
+	 * @param  bool 	$clean 	Whether to remove any untracked files.
+	 */
+	public function reset( $mode = '--hard', $path = 'HEAD', $clean = false ) {
+		$this->run( "reset $mode $path" );
+		if ( $clean === true ) {
+			$this->run( 'clean -f -d' );
+		}
+	}
+
+	/**
+	 * Reverts the working directory to a specified commit.
+	 * @access public
+	 * @param  string $commit The hash of the commit to revert to.
+	 */
+	public function revert( $commit ) {
+		$this->reset( '--hard', 'HEAD', true );
+		$this->reset( '--hard', $commit );
+		$this->reset( '--soft', 'HEAD@{1}' );
+	}
+
+	/**
+	 * Executes a Git command.
+	 * @access public
+	 * @param  string 	$command 		The git command to execute.
+	 * @param  string 	$callback 	    The function to callback on response.
+	 * @param  string 	$args 			Optional additional arguements to pass to the callback.
+	 */
+	public function run( $command, $callback = '', $args = '' ) {
+		
+		// Run the actual Git command.
+		$cmd = escapeshellcmd( "git $command" );
+		$dir = getcwd();
+		chdir( $this->dir );
+		exec( $cmd, $output, $error );
+		chdir( $dir );
+		
+		// If using a callback, initiate the callback class and call the function.
+		if ( $callback != '' ) {
+			$response 			= new Revisr_Git_Callback;
+			$success_callback 	= 'success_' . $callback;
+			$failure_callback 	= 'null_' . $callback;
+			if ( $error ) {
+				return $response->$failure_callback( $error, $args );
+			} else {
+				return $response->$success_callback( $output, $args );
+			}
+		}
+
+		// If not using a callback, return the output (or false on failure).
+		if ( ! $error ) {
+			return $output;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Stages the array of files passed through the New Commit screen.
+	 * @access public
+	 * @param  array $staged_files The files to add/remove
+	 */
+	public function stage_files( $staged_files ) {
+		$errors = array();
+		
+		foreach ( $staged_files as $result ) {
+			$file 	= substr( $result, 3 );
+			$status = Revisr_Git::get_status( substr( $result, 0, 2 ) );
+			
+			if ( $status == __( 'Deleted', 'revisr' ) ) {
+				if ( $this->run( 'rm "' . $file . '"' ) === false ) {
+					$errors[] = $file;
+				}
+			} else {
+				if ( $this->run( 'add "' . $file . '"' ) === false ) {
+					$errors[] = $file;
+				}
+			}
+		}
+
+		if ( ! empty( $errors ) ) {
+			$msg = __( 'There was an error staging the files. Please check the settings and try again.', 'revisr' );
+			Revisr_Admin::alert( $msg, true );
+			Revisr_Admin::log( __( 'Error staging files.', 'revisr' ), 'error' );
+		}
+	}	
+
+	/**
+	 * Returns the current status.
+	 * @access public
+	 * @param  string $args Defaults to "--short".
+	 */
+	public function status( $args = '--short' ) {
+		$status = $this->run( "status $args" );
+		return $status;
+	}
+
+	/**
+	 * Adds a tag to the repository, or returns a list of tags if no parameters are passed.
+	 * @access public
+	 * @param  string $tag 		The tag to add.
+	 */
+	public function tag( $tag = '' ) {
+		$tag = $this->run( "tag $tag" );
+		return $tag;
+	}
+
+	/**
+	 * Pings a remote repository to verify that it exists and is reachable.
+	 * @access public
+	 * @param  string $remote The remote to ping.
+	 */
+	public function verify_remote( $remote = '' ) {
+		if ( $remote != '' ) {
+			$ping = $this->run( "ls-remote $remote HEAD", __FUNCTION__ );
+		} else {
+			$ping = $this->run( "ls-remote " . $_REQUEST['remote'] . " HEAD", __FUNCTION__ );
+		}
+		return $ping;
+	}
+
+	/**
+	 * Returns the current version of Git.
+	 * @access public
+	 */
+	public function version() {
+		$version = $this->run( 'version', __FUNCTION__ );
+		return $version;
+	}
+}
